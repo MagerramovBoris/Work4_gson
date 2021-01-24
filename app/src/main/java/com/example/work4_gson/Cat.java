60880d9012385589a603cdfad703fecdd1c7550a
MagerramovBoris <magerramov.boris.s@gmail.com>
2021-01-24 21:34:58 +0700

    Пункт 3. Добавил класс Cat

---
+++ app/src/main/java/com/example/work4_gson/Cat.java
@@ -0,0 +1,14 @@
+package com.example.work4_gson;
+
+public class Cat {
+    // Переменные класс
+    public String name;
+    public int age;
+    public int color;
+    // Конструктор класса
+    public Cat(String name, int age, int color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+}
